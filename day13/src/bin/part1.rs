660c6e6e6c8c4e06b936cb4a1f54e90fa81fd4af
Bob Matcuk <bmatcuk@gmail.com>
2019-12-13 11:40:10 -0500

    day 13

---
+++ day13/src/bin/part1.rs
@@ -0,0 +1,356 @@
+//! --- Day 13: Care Package ---
+//!
+//! As you ponder the solitude of space and the ever-increasing three-hour roundtrip for messages
+//! between you and Earth, you notice that the Space Mail Indicator Light is blinking. To help keep
+//! you sane, the Elves have sent you a care package.
+//!
+//! It's a new game for the ship's arcade cabinet! Unfortunately, the arcade is all the way on the
+//! other end of the ship. Surely, it won't be hard to build your own - the care package even comes
+//! with schematics.
+//!
+//! The arcade cabinet runs Intcode software like the game the Elves sent (your puzzle input). It
+//! has a primitive screen capable of drawing square tiles on a grid. The software draws tiles to
+//! the screen with output instructions: every three output instructions specify the x position
+//! (distance from the left), y position (distance from the top), and tile id. The tile id is
+//! interpreted as follows:
+//!
+//!     0 is an empty tile. No game object appears in this tile.
+//!     1 is a wall tile. Walls are indestructible barriers.
+//!     2 is a block tile. Blocks can be broken by the ball.
+//!     3 is a horizontal paddle tile. The paddle is indestructible.
+//!     4 is a ball tile. The ball moves diagonally and bounces off objects.
+//!
+//! For example, a sequence of output values like 1,2,3,6,5,4 would draw a horizontal paddle tile
+//! (1 tile from the left and 2 tiles from the top) and a ball tile (6 tiles from the left and 5
+//! tiles from the top).
+//!
+//! Start the game. How many block tiles are on the screen when the game exits?
+
+use std::clone::Clone;
+use std::cmp::PartialEq;
+use std::convert::{TryFrom, TryInto};
+use std::error::Error;
+use std::fmt::{self, Display};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str;
+
+#[derive(Debug)]
+struct InvalidParameterMode(i64);
+
+impl Display for InvalidParameterMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is not a valid parameter mode.", self.0)
+    }
+}
+
+impl Error for InvalidParameterMode {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug)]
+struct InvalidOpcode(i64);
+
+impl Display for InvalidOpcode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is not a valid opcode.", self.0)
+    }
+}
+
+impl Error for InvalidOpcode {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug)]
+struct InvalidOutputMode(ParameterMode);
+
+impl Display for InvalidOutputMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is not a valid parameter mode for output.", self.0)
+    }
+}
+
+impl Error for InvalidOutputMode {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum ParameterMode {
+    Position,
+    Immediate,
+    Relative,
+}
+
+impl Display for ParameterMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ParameterMode::Position => "'position'",
+                ParameterMode::Immediate => "'immediate'",
+                ParameterMode::Relative => "'relative'",
+            }
+        )
+    }
+}
+
+impl TryFrom<i64> for ParameterMode {
+    type Error = InvalidParameterMode;
+
+    fn try_from(code: i64) -> Result<Self, Self::Error> {
+        match code {
+            0 => Ok(ParameterMode::Position),
+            1 => Ok(ParameterMode::Immediate),
+            2 => Ok(ParameterMode::Relative),
+            _ => Err(InvalidParameterMode(code)),
+        }
+    }
+}
+
+impl ParameterMode {
+    fn modes2(code: i64) -> Result<(Self, Self), InvalidParameterMode> {
+        Ok(((code % 10).try_into()?, ((code / 10) % 10).try_into()?))
+    }
+
+    fn modes3(code: i64) -> Result<(Self, Self, Self), InvalidParameterMode> {
+        Ok((
+            (code % 10).try_into()?,
+            ((code / 10) % 10).try_into()?,
+            ((code / 100) % 10).try_into()?,
+        ))
+    }
+}
+
+enum Opcode {
+    Add(ParameterMode, ParameterMode, ParameterMode),
+    Multiply(ParameterMode, ParameterMode, ParameterMode),
+    Input(ParameterMode),
+    Output(ParameterMode),
+    JumpIfTrue(ParameterMode, ParameterMode),
+    JumpIfFalse(ParameterMode, ParameterMode),
+    LessThan(ParameterMode, ParameterMode, ParameterMode),
+    Equal(ParameterMode, ParameterMode, ParameterMode),
+    RelativeBaseOffset(ParameterMode),
+    Halt,
+}
+
+impl TryFrom<i64> for Opcode {
+    type Error = InvalidOpcode;
+
+    fn try_from(code: i64) -> Result<Self, Self::Error> {
+        match code % 100 {
+            1 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Add(mode1, mode2, mode3))
+            }
+            2 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Multiply(mode1, mode2, mode3))
+            }
+            3 => {
+                let mode = (code / 100).try_into().map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Input(mode))
+            }
+            4 => {
+                let mode = (code / 100).try_into().map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Output(mode))
+            }
+            5 => {
+                let (mode1, mode2) =
+                    ParameterMode::modes2(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::JumpIfTrue(mode1, mode2))
+            }
+            6 => {
+                let (mode1, mode2) =
+                    ParameterMode::modes2(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::JumpIfFalse(mode1, mode2))
+            }
+            7 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::LessThan(mode1, mode2, mode3))
+            }
+            8 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Equal(mode1, mode2, mode3))
+            }
+            9 => {
+                let mode = (code / 100).try_into().map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::RelativeBaseOffset(mode))
+            }
+            99 => Ok(Opcode::Halt),
+            _ => Err(InvalidOpcode(code)),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Intcode {
+    code: Vec<i64>,
+    ip: usize,
+    relative_base: isize,
+}
+
+impl Intcode {
+    fn load<R: BufRead>(reader: R) -> Result<Intcode, Box<dyn Error>> {
+        let code = reader
+            .split(b',')
+            .map(|code| match code {
+                Ok(code) => {
+                    let s = str::from_utf8(&code)?;
+                    Ok(s.trim().parse()?)
+                }
+                Err(e) => Err(Box::new(e) as Box<dyn Error>),
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Intcode {
+            code,
+            ip: 0,
+            relative_base: 0,
+        })
+    }
+
+    fn read(&mut self, i: usize) -> i64 {
+        if i >= self.code.len() {
+            self.code.resize(i + 1, 0);
+        }
+        self.code[i]
+    }
+
+    fn write(&mut self, i: usize, v: i64) {
+        if i >= self.code.len() {
+            self.code.resize(i + 1, 0);
+        }
+        self.code[i] = v;
+    }
+
+    fn output_operand(
+        &mut self,
+        sp: usize,
+        mode: ParameterMode,
+    ) -> Result<usize, InvalidOutputMode> {
+        let v = self.code[sp];
+        match mode {
+            ParameterMode::Position => Ok(v as usize),
+            ParameterMode::Immediate => Err(InvalidOutputMode(mode)),
+            ParameterMode::Relative => Ok((v as isize + self.relative_base) as usize),
+        }
+    }
+
+    fn operand(&mut self, sp: usize, mode: ParameterMode) -> i64 {
+        let v = self.code[sp];
+        match mode {
+            ParameterMode::Position => self.read(v as usize),
+            ParameterMode::Immediate => v,
+            ParameterMode::Relative => self.read((v as isize + self.relative_base) as usize),
+        }
+    }
+
+    fn operands2(&mut self, sp: usize, mode1: ParameterMode, mode2: ParameterMode) -> (i64, i64) {
+        (self.operand(sp, mode1), self.operand(sp + 1, mode2))
+    }
+
+    fn run(&mut self, input: &Vec<i64>) -> Result<(Vec<i64>, bool), Box<dyn Error>> {
+        let mut output: Vec<i64> = Vec::new();
+        let mut inputp = 0;
+        let mut halted = false;
+        loop {
+            let opcode: Opcode = self.read(self.ip).try_into()?;
+            self.ip = match opcode {
+                Opcode::Add(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, op1 + op2);
+                    self.ip + 4
+                }
+                Opcode::Multiply(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, op1 * op2);
+                    self.ip + 4
+                }
+                Opcode::Input(mode) => {
+                    if inputp >= input.len() {
+                        // not enough input; suspend
+                        break;
+                    }
+
+                    let op = self.output_operand(self.ip + 1, mode)?;
+                    self.write(op, input[inputp]);
+                    inputp += 1;
+                    self.ip + 2
+                }
+                Opcode::Output(mode) => {
+                    let op = self.operand(self.ip + 1, mode);
+                    output.push(op);
+                    self.ip + 2
+                }
+                Opcode::JumpIfTrue(mode1, mode2) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    if op1 != 0 {
+                        op2 as usize
+                    } else {
+                        self.ip + 3
+                    }
+                }
+                Opcode::JumpIfFalse(mode1, mode2) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    if op1 == 0 {
+                        op2 as usize
+                    } else {
+                        self.ip + 3
+                    }
+                }
+                Opcode::LessThan(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, if op1 < op2 { 1 } else { 0 });
+                    self.ip + 4
+                }
+                Opcode::Equal(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, if op1 == op2 { 1 } else { 0 });
+                    self.ip + 4
+                }
+                Opcode::RelativeBaseOffset(mode) => {
+                    let op = self.operand(self.ip + 1, mode);
+                    self.relative_base += op as isize;
+                    self.ip + 2
+                }
+                Opcode::Halt => {
+                    halted = true;
+                    break;
+                }
+            };
+        }
+        Ok((output, halted))
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+    let mut program = Intcode::load(reader)?;
+    let input = vec![];
+    let (output, _) = program.run(&input)?;
+    let result = output
+        .iter()
+        .skip(2)
+        .step_by(3)
+        .filter(|&&i| i == 2)
+        .count();
+    println!("Blocks: {}", result);
+
+    Ok(())
+}
---
+++ day13/src/bin/part2.rs
@@ -0,0 +1,414 @@
+//! --- Part Two ---
+//!
+//! The game didn't run because you didn't put in any quarters. Unfortunately, you did not bring
+//! any quarters. Memory address 0 represents the number of quarters that have been inserted; set
+//! it to 2 to play for free.
+//!
+//! The arcade cabinet has a joystick that can move left and right. The software reads the position
+//! of the joystick with input instructions:
+//!
+//!     If the joystick is in the neutral position, provide 0.
+//!     If the joystick is tilted to the left, provide -1.
+//!     If the joystick is tilted to the right, provide 1.
+//!
+//! The arcade cabinet also has a segment display capable of showing a single number that
+//! represents the player's current score. When three output instructions specify X=-1, Y=0, the
+//! third output instruction is not a tile; the value instead specifies the new score to show in
+//! the segment display. For example, a sequence of output values like -1,0,12345 would show 12345
+//! as the player's current score.
+//!
+//! Beat the game by breaking all the blocks. What is your score after the last block is broken?
+
+use std::clone::Clone;
+use std::cmp::PartialEq;
+use std::convert::{TryFrom, TryInto};
+use std::error::Error;
+use std::fmt::{self, Display};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str;
+use std::thread::sleep;
+use std::time::Duration;
+
+#[derive(Debug)]
+struct InvalidParameterMode(i64);
+
+impl Display for InvalidParameterMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is not a valid parameter mode.", self.0)
+    }
+}
+
+impl Error for InvalidParameterMode {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug)]
+struct InvalidOpcode(i64);
+
+impl Display for InvalidOpcode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is not a valid opcode.", self.0)
+    }
+}
+
+impl Error for InvalidOpcode {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug)]
+struct InvalidOutputMode(ParameterMode);
+
+impl Display for InvalidOutputMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is not a valid parameter mode for output.", self.0)
+    }
+}
+
+impl Error for InvalidOutputMode {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum ParameterMode {
+    Position,
+    Immediate,
+    Relative,
+}
+
+impl Display for ParameterMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ParameterMode::Position => "'position'",
+                ParameterMode::Immediate => "'immediate'",
+                ParameterMode::Relative => "'relative'",
+            }
+        )
+    }
+}
+
+impl TryFrom<i64> for ParameterMode {
+    type Error = InvalidParameterMode;
+
+    fn try_from(code: i64) -> Result<Self, Self::Error> {
+        match code {
+            0 => Ok(ParameterMode::Position),
+            1 => Ok(ParameterMode::Immediate),
+            2 => Ok(ParameterMode::Relative),
+            _ => Err(InvalidParameterMode(code)),
+        }
+    }
+}
+
+impl ParameterMode {
+    fn modes2(code: i64) -> Result<(Self, Self), InvalidParameterMode> {
+        Ok(((code % 10).try_into()?, ((code / 10) % 10).try_into()?))
+    }
+
+    fn modes3(code: i64) -> Result<(Self, Self, Self), InvalidParameterMode> {
+        Ok((
+            (code % 10).try_into()?,
+            ((code / 10) % 10).try_into()?,
+            ((code / 100) % 10).try_into()?,
+        ))
+    }
+}
+
+enum Opcode {
+    Add(ParameterMode, ParameterMode, ParameterMode),
+    Multiply(ParameterMode, ParameterMode, ParameterMode),
+    Input(ParameterMode),
+    Output(ParameterMode),
+    JumpIfTrue(ParameterMode, ParameterMode),
+    JumpIfFalse(ParameterMode, ParameterMode),
+    LessThan(ParameterMode, ParameterMode, ParameterMode),
+    Equal(ParameterMode, ParameterMode, ParameterMode),
+    RelativeBaseOffset(ParameterMode),
+    Halt,
+}
+
+impl TryFrom<i64> for Opcode {
+    type Error = InvalidOpcode;
+
+    fn try_from(code: i64) -> Result<Self, Self::Error> {
+        match code % 100 {
+            1 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Add(mode1, mode2, mode3))
+            }
+            2 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Multiply(mode1, mode2, mode3))
+            }
+            3 => {
+                let mode = (code / 100).try_into().map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Input(mode))
+            }
+            4 => {
+                let mode = (code / 100).try_into().map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Output(mode))
+            }
+            5 => {
+                let (mode1, mode2) =
+                    ParameterMode::modes2(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::JumpIfTrue(mode1, mode2))
+            }
+            6 => {
+                let (mode1, mode2) =
+                    ParameterMode::modes2(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::JumpIfFalse(mode1, mode2))
+            }
+            7 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::LessThan(mode1, mode2, mode3))
+            }
+            8 => {
+                let (mode1, mode2, mode3) =
+                    ParameterMode::modes3(code / 100).map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::Equal(mode1, mode2, mode3))
+            }
+            9 => {
+                let mode = (code / 100).try_into().map_err(|_| InvalidOpcode(code))?;
+                Ok(Opcode::RelativeBaseOffset(mode))
+            }
+            99 => Ok(Opcode::Halt),
+            _ => Err(InvalidOpcode(code)),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Intcode {
+    code: Vec<i64>,
+    ip: usize,
+    relative_base: isize,
+}
+
+impl Intcode {
+    fn load<R: BufRead>(reader: R) -> Result<Intcode, Box<dyn Error>> {
+        let code = reader
+            .split(b',')
+            .map(|code| match code {
+                Ok(code) => {
+                    let s = str::from_utf8(&code)?;
+                    Ok(s.trim().parse()?)
+                }
+                Err(e) => Err(Box::new(e) as Box<dyn Error>),
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Intcode {
+            code,
+            ip: 0,
+            relative_base: 0,
+        })
+    }
+
+    fn read(&mut self, i: usize) -> i64 {
+        if i >= self.code.len() {
+            self.code.resize(i + 1, 0);
+        }
+        self.code[i]
+    }
+
+    fn write(&mut self, i: usize, v: i64) {
+        if i >= self.code.len() {
+            self.code.resize(i + 1, 0);
+        }
+        self.code[i] = v;
+    }
+
+    fn output_operand(
+        &mut self,
+        sp: usize,
+        mode: ParameterMode,
+    ) -> Result<usize, InvalidOutputMode> {
+        let v = self.code[sp];
+        match mode {
+            ParameterMode::Position => Ok(v as usize),
+            ParameterMode::Immediate => Err(InvalidOutputMode(mode)),
+            ParameterMode::Relative => Ok((v as isize + self.relative_base) as usize),
+        }
+    }
+
+    fn operand(&mut self, sp: usize, mode: ParameterMode) -> i64 {
+        let v = self.code[sp];
+        match mode {
+            ParameterMode::Position => self.read(v as usize),
+            ParameterMode::Immediate => v,
+            ParameterMode::Relative => self.read((v as isize + self.relative_base) as usize),
+        }
+    }
+
+    fn operands2(&mut self, sp: usize, mode1: ParameterMode, mode2: ParameterMode) -> (i64, i64) {
+        (self.operand(sp, mode1), self.operand(sp + 1, mode2))
+    }
+
+    fn run(&mut self, input: &Vec<i64>) -> Result<(Vec<i64>, bool), Box<dyn Error>> {
+        let mut output: Vec<i64> = Vec::new();
+        let mut inputp = 0;
+        let mut halted = false;
+        loop {
+            let opcode: Opcode = self.read(self.ip).try_into()?;
+            self.ip = match opcode {
+                Opcode::Add(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, op1 + op2);
+                    self.ip + 4
+                }
+                Opcode::Multiply(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, op1 * op2);
+                    self.ip + 4
+                }
+                Opcode::Input(mode) => {
+                    if inputp >= input.len() {
+                        // not enough input; suspend
+                        break;
+                    }
+
+                    let op = self.output_operand(self.ip + 1, mode)?;
+                    self.write(op, input[inputp]);
+                    inputp += 1;
+                    self.ip + 2
+                }
+                Opcode::Output(mode) => {
+                    let op = self.operand(self.ip + 1, mode);
+                    output.push(op);
+                    self.ip + 2
+                }
+                Opcode::JumpIfTrue(mode1, mode2) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    if op1 != 0 {
+                        op2 as usize
+                    } else {
+                        self.ip + 3
+                    }
+                }
+                Opcode::JumpIfFalse(mode1, mode2) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    if op1 == 0 {
+                        op2 as usize
+                    } else {
+                        self.ip + 3
+                    }
+                }
+                Opcode::LessThan(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, if op1 < op2 { 1 } else { 0 });
+                    self.ip + 4
+                }
+                Opcode::Equal(mode1, mode2, mode3) => {
+                    let (op1, op2) = self.operands2(self.ip + 1, mode1, mode2);
+                    let op3 = self.output_operand(self.ip + 3, mode3)?;
+                    self.write(op3 as usize, if op1 == op2 { 1 } else { 0 });
+                    self.ip + 4
+                }
+                Opcode::RelativeBaseOffset(mode) => {
+                    let op = self.operand(self.ip + 1, mode);
+                    self.relative_base += op as isize;
+                    self.ip + 2
+                }
+                Opcode::Halt => {
+                    halted = true;
+                    break;
+                }
+            };
+        }
+        Ok((output, halted))
+    }
+}
+
+struct Point2(usize, usize);
+
+fn update_display(
+    score: &mut i64,
+    display: &mut Vec<Vec<u8>>,
+    output: &Vec<i64>,
+) -> (Point2, Point2) {
+    let mut ball = Point2(0, 0);
+    let mut paddle = Point2(0, 0);
+    for out in output.chunks(3) {
+        if out[0] == -1 && out[1] == 0 {
+            *score = out[2];
+            continue;
+        }
+
+        let x = out[0] as usize;
+        let y = out[1] as usize;
+        display[y][x] = out[2] as u8;
+
+        if out[2] == 3 {
+            paddle = Point2(x, y);
+        } else if out[2] == 4 {
+            ball = Point2(x, y);
+        }
+    }
+    (ball, paddle)
+}
+
+fn draw_display(display: &Vec<Vec<u8>>, score: i64) {
+    println!("\x1b[H");
+    println!("Score: {}", score);
+    for row in display {
+        println!(
+            "{}",
+            row.iter()
+                .map(|id| match id {
+                    1 => '█',
+                    2 => '░',
+                    3 => '―',
+                    4 => '⍟',
+                    _ => ' ',
+                })
+                .collect::<String>()
+        );
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+    let mut program = Intcode::load(reader)?;
+    program.code[0] = 2;
+    println!("\x1b[?1049h");
+
+    // board dimensions are 45x24
+    let mut display = vec![vec![0u8; 45]; 24];
+    let mut score = 0;
+
+    // run once to get a lay of the land before providing our first input
+    let mut input = Vec::with_capacity(1);
+    let (output, mut halted) = program.run(&input)?;
+    let (ball, paddle) = update_display(&mut score, &mut display, &output);
+    draw_display(&display, score);
+    input.push(((ball.0 as i64) - (paddle.0 as i64)).signum());
+
+    // now loop until the game has halted
+    while !halted {
+        let (output, halt) = program.run(&input)?;
+        let (ball, paddle) = update_display(&mut score, &mut display, &output);
+        sleep(Duration::from_millis(1));
+        draw_display(&display, score);
+        input[0] = ((ball.0 as i64) - (paddle.0 as i64)).signum();
+        halted = halt;
+    }
+
+    sleep(Duration::from_millis(500));
+    println!("\x1b[?1049l");
+    Ok(())
+}
